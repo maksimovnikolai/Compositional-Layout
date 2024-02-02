52f688ba1da7785f9b80efb94eb505f8bec9c04b
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-02 21:47:01 +0300

    setup project

---
+++ Compositional-Layout/Compositional-Layout/ViewControllers/CollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewController.swift
+//  Compositional-Layout
+//
+//  Created by Nikolai Maksimov on 02.02.2024.
+//
+
+import UIKit
+
+final class CollectionViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
