7d1b3c3921ed8f5cf90a6c3b95a953a98e3f41f3
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-03 14:12:30 +0300

    setup multiple section project

---
+++ Multiple-Sections/Multiple-Sections/ViewControllers/MultipleSectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MultipleSectionViewController.swift
+//  Multiple-Sections
+//
+//  Created by Nikolai Maksimov on 03.02.2024.
+//
+
+import UIKit
+
+class MultipleSectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
