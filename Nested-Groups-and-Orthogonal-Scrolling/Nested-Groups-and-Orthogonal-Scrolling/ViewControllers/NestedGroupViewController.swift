0d6bac311858cba4e5b9ba68e94b8d5cc511e5d0
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-02-03 18:42:39 +0300

    setup project

---
+++ Nested-Groups-and-Orthogonal-Scrolling/Nested-Groups-and-Orthogonal-Scrolling/ViewControllers/NestedGroupViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NestedGroupViewController.swift
+//  Nested-Groups-and-Orthogonal-Scrolling
+//
+//  Created by Nikolai Maksimov on 03.02.2024.
+//
+
+import UIKit
+
+final class NestedGroupViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
